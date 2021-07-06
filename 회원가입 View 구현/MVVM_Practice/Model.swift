2b07165c77d57aba90e31ecdbbdbde224a371425
JunseokNoh <40800157+JunseokNoh@users.noreply.github.com>
2021-07-06 16:30:29 +0900

    회원가입 View 구현

---
+++ 회원가입 View 구현/MVVM_Practice/Model.swift
@@ -0,0 +1,11 @@
+//
+//  Model.swift
+//  MVVM_Practice
+//
+//  Created by junseok on 2021/07/05.
+//
+
+struct Person {
+    var name: String
+    var age: Int
+}
---
+++ 회원가입 View 구현/MVVM_Practice/ViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ViewModel.swift
+//  MVVM_Practice
+//
+//  Created by junseok on 2021/07/05.
+//
+
+import Foundation
+import UIKit
+
+struct PersonViewModel {
+    typealias Listener = (Person) -> Void
+    var listener: Listener?
+    
+    var person: Person {
+        didSet {
+            listener?(person)
+        }
+    }
+
+    mutating func bind(listener: Listener?) {
+        self.listener = listener
+    }
+}
